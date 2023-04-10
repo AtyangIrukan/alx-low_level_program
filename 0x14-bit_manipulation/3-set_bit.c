ee20aadb47a802d55f93fd2a78714f35d1e9d704
AtyangIrukan <bridgetsharon10@gmail.com>
2023-04-10 11:32:59 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,14 +7,14 @@
  * @index: index position to change, starting from 0
  * Return: 1 if it worked, -1 if error
  */
-int set_bit(unsigned long int *n, unsigned int number)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int m;
 
-	if (number > 64)
+	if (index > 64)
 		return (-1);
 
-	for (m = 1; number > 0; number--, m *= 2)
+	for (m = 1; index > 0; index--, m *= 2)
 		;
 	*n += m;
 
