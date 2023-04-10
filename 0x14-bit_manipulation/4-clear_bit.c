a6f12c3080094df8d8c61eaef3f6363cf5d7e160
AtyangIrukan <bridgetsharon10@gmail.com>
2023-04-10 11:35:32 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,18 +6,18 @@
  * @index: index position to change
  * Return: 1 if it worked, -1 if error
  */
-int clear_bit(unsigned long int *n, unsigned int tobeclear)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int nn;
 	unsigned int val;
 
-	if (tobeclear > 64)
+	if (index > 64)
 		return (-1);
-	val = tobeclear;
+	val = index;
 	for (nn = 1; val > 0; nn *= 2, val--)
 		;
 
-	if ((*n >> tobeclear) & 1)
+	if ((*n >> index) & 1)
 		*n -= nn;
 
 	return (1);
